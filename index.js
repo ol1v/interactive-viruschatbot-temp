11ede49c4611b43acbc5442cc45d791a4652fee9
Oliver Lennartsson <olilen@u5cg8135050>
2020-10-08 15:31:04 +0200

    added url64-enc and decoding for URL

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 const builder = require('botbuilder');
 const restify = require('restify');
+const fetch = require("node-fetch");
       
 const connector = new builder.ChatConnector();
 const bot = new builder.UniversalBot(
     connector,
     [
         (session) => {
-            session.send('Use the lookup command and paste a URL <lookup http://www.example.com> to se if the URL is malicious');
+            if(session.message.text.includes('lookup')) {
+                let url = session.message.text.replace('lookup','')
+                session.send('Looking up ' + url)
+
+                //Convert url to base64 string
+                const url_encoded = convert_to_base64(url)
+                
+                //Breakout to function when finished TBC
+                fetch('https://www.virustotal.com/api/v3/urls/' + url_encoded, {
+                    method: 'GET',
+                    headers: {
+                        'x-apikey': 'your-api-key-here'
+                    }
+                }).then(response => response.json())
+                  .then(data => console.log(data))
+                    // Do stuff with data here
+                               
+            } else {
+                session.send('Try: <lookup www.exampleurl.com>');
+            }
         }
     ]
 ).set('storage', new builder.MemoryBotStorage());
 
-const dialog = new builder.IntentDialog();
+function convert_to_base64(url) {
+    //encoding url
+    const buf = Buffer.from(url, 'utf8')
 
-dialog.matches(/^lookup/i, [
-    function (session, args, next) {
-        console.log(session);
-        var conversationId =
-    session.message.address.conversation.id;
-        session.send('Looking up...')
-    }
-]);
-      
+    // return base64 decoded string and strip =
+    return buf.toString('base64').replace('=','')
+}
+
 const server = restify.createServer();
 server.post('/api/messages', connector.listen());
 server.listen(3978);
 console.log('Listening on port 3978');
+
